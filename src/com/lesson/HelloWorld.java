7d175d3c44469cadfa10a84135560bab61ef0f68
max <bogovick@gmail.com>
2019-07-22 06:17:15 +0300

    fix run class 4

--- src/com/lesson/HelloWorld.java
+++ src/com/lesson/HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
-		System.out.println("1");
+		System.out.println("10");
+		System.out.println("new Line");
+		System.out.println("new Line");
 		System.out.println("new Line");
 	}
 }
